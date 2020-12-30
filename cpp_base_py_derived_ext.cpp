6c9a6d6bc72d5c1b8609724433259c5b47178680
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2020-12-30 10:11:43 -0800

    replacing prints with int returns, checked with asserts

--- cpp_base_py_derived_ext.cpp
+++ cpp_base_py_derived_ext.cpp
@@ -1,43 +1,47 @@
 #include <boost/python/call_method.hpp>
 #include <boost/python/class.hpp>
 #include <boost/python/def.hpp>
-#include <boost/python/manage_new_object.hpp>
 #include <boost/python/module.hpp>
-#include <boost/python/return_value_policy.hpp>
 
-#include <iostream>
 #include <memory>
-#include <string>
 
 namespace {
 
 namespace py = boost::python;
 
-inline void to_cout(std::string msg) { std::cout << msg << std::endl; }
-
 struct base {
-  virtual void show() const {
-    to_cout("base::show");
-  }
+  base() : base_num(100) {}
+
+  virtual int get_num() const { return base_num; }
 
   virtual std::shared_ptr<base> clone() const {
-    to_cout("base::clone");
-    return std::shared_ptr<base>(new base);
+    return std::shared_ptr<base>(new base(150));
   }
 
   virtual ~base() = default;
+
+ private:
+  explicit base(int num) : base_num(num) {}
+  int base_num;
 };
+
+inline int get_num(std::shared_ptr<base> b) { return b->get_num(); }
+
+inline int clone_get_num(std::shared_ptr<base> b) {
+  std::shared_ptr<base> c = b->clone();
+  return (b->get_num() + 3) * 1000 + (c->get_num() + 7);
+}
 
 struct base_callback : public base {
   PyObject* self;
   base_callback(PyObject* self_) : self(self_) {}
   base_callback(PyObject* self_, const base&) : self(self_) {}
 
-  void show() const override {
-    return py::call_method<void>(self, "show");
+  int get_num() const override {
+    return py::call_method<int>(self, "get_num");
   }
-  static void default_show(std::shared_ptr<base> b) {
-    b->base::show();
+  static int default_get_num(std::shared_ptr<base> b) {
+    return b->base::get_num();
   }
 
   std::shared_ptr<base> clone() const override {
@@ -48,29 +52,18 @@ struct base_callback : public base {
   }
 };
 
-inline void show(std::shared_ptr<base> b) { b->show(); }
-
-inline void clone_show(std::shared_ptr<base> b) {
-  to_cout("cpp b->clone()");
-  std::shared_ptr<base> c = b->clone();
-  to_cout("cpp c->show()");
-  c->show();
-}
-
 }
 
 BOOST_PYTHON_MODULE(rwgk_tbx_cpp_base_py_derived_ext)
 {
   namespace py = boost::python;
 
-  py::def("to_cout", to_cout);
-
   py::class_<base, std::shared_ptr<base_callback>>("base")
     .def(py::init<>())
-    .def("show", &base_callback::default_show)
+    .def("get_num", &base_callback::default_get_num)
     .def("clone", &base_callback::default_clone)
   ;
 
-  py::def("show", show);
-  py::def("clone_show", clone_show);
+  py::def("get_num", get_num);
+  py::def("clone_get_num", clone_get_num);
 }
--- tst_cpp_base_py_derived.py
+++ tst_cpp_base_py_derived.py
@@ -6,34 +6,55 @@ import sys
 
 class drvd(m.base):
 
-  def __init__(self):
+  def __init__(self, _num = 200):
     super().__init__()
+    self._drvd_num = _num
 
-  def show(self):
-    m.to_cout("drvd.show")
+  def get_num(self):
+    return self._drvd_num
 
   def clone(self):
-    m.to_cout("drvd.clone")
-    return drvd()
+    return drvd(250)
 
 
-def test_main():
+def expect_type_error(callable):
+  try:
+    callable()
+  except TypeError as e:
+    assert str(e).startswith(
+        "No to_python (by-value) converter found for C++ type: ")
+    #   "std::shared_ptr<(anonymous namespace)::base>"
+    # may be name-mangled, therefore only checking for pieces:
+    assert "shared" in str(e)
+    assert "base" in str(e)
+  else:
+    raise RuntimeError("TypeError not raised.")
+
+
+def test_base():
   b = m.base()
-  b.show()
-  m.show(b)
-  #b.clone()
-  #m.clone_show(b)
+  assert b.get_num() == 100
+  m.get_num(b) == 100
+  # bc = b.clone()
+  # assert bc.get_num() == 150
+  # assert m.clone_get_num(b) == 103157
+  expect_type_error(b.clone)
+  expect_type_error(lambda b=b: m.clone_get_num(b))
 
+
+def test_drvd():
   d = drvd()
-  d.show()
-  m.show(d)
-  d.clone()
-  m.clone_show(d)
+  assert d.get_num() == 200
+  assert m.get_num(d) == 200
+  dc = d.clone()
+  assert dc.get_num() == 250
+  assert m.clone_get_num(d) == 203257
 
 
 def run(args):
   assert not args
-  test_main()
+  test_base()
+  test_drvd()
   print("Done.")
 
 
